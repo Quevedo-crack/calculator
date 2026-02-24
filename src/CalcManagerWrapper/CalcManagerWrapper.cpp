9b98c4cfc4466ec527e4ec3fdc93ae49b0b86744
Quevedo-crack <quevedotropical@gmail.com>
2026-02-24 12:31:49 -0500

    Create CalcManagerWrapper.cpp

---
+++ src/CalcManagerWrapper/CalcManagerWrapper.cpp
@@ -0,0 +1,21 @@
+#include "CalcManagerWrapper.h"
+#include "..\\CalcManager\\CalculatorManager.h"  // cabecera nativa
+
+using namespace CalcManagerWrapper;
+
+StandardCalculatorManagerWrapper::StandardCalculatorManagerWrapper()
+{
+    // Inicialización del motor nativo si hace falta
+}
+
+void StandardCalculatorManagerWrapper::Init()
+{
+    // Aquí llamarías a la inicialización real del motor nativo
+    // Ejemplo: StandardCalculatorManager::Initialize();
+}
+
+int StandardCalculatorManagerWrapper::Add(int a, int b)
+{
+    // Ejemplo simple: en realidad deberías llamar a la función nativa
+    return a + b;
+}
