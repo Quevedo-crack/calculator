12d14594392cbb11dd62f27af71f37322c7c5fce
Quevedo-crack <quevedotropical@gmail.com>
2026-02-24 13:00:04 -0500

    Update CalcManagerWrapper.cpp

--- src/CalcManagerWrapper/CalcManagerWrapper.cpp
+++ src/CalcManagerWrapper/CalcManagerWrapper.cpp
@@ -1,38 +1,26 @@
-#include "CalcManagerWrapper.h"
-#include "..\\CalcManager\\CalculatorManager.h"
-#include <msclr/marshal_cppstd.h>
-
-using namespace CalcManagerWrapper;
-using namespace msclr::interop;
-
-StandardCalculatorManagerWrapper::StandardCalculatorManagerWrapper()
-{
-    nativeManager = new StandardCalculatorManager();
-}
-
-StandardCalculatorManagerWrapper::~StandardCalculatorManagerWrapper()
-{
-    delete nativeManager;
-}
-
-void StandardCalculatorManagerWrapper::Init()
-{
-    nativeManager->Initialize();
-}
-
-void StandardCalculatorManagerWrapper::ProcessCommand(String^ command)
-{
-    std::wstring cmd = marshal_as<std::wstring>(command);
-    nativeManager->ProcessCommand(cmd);
-}
-
-String^ StandardCalculatorManagerWrapper::GetDisplayText()
-{
-    std::wstring text = nativeManager->GetDisplayText();
-    return gcnew String(text.c_str());
-}
-
-void StandardCalculatorManagerWrapper::Clear()
-{
-    nativeManager->Clear();
-}
+<Window x:Class="Calculator.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="Uno Calculator" Height="400" Width="300">
+    <Grid>
+        <TextBox x:Name="Display" FontSize="24" HorizontalAlignment="Stretch" VerticalAlignment="Top" Height="50" IsReadOnly="True" TextAlignment="Right"/>
+        <UniformGrid Rows="5" Columns="4" Margin="0,60,0,0">
+            <Button Content="7" Click="Button_Click"/>
+            <Button Content="8" Click="Button_Click"/>
+            <Button Content="9" Click="Button_Click"/>
+            <Button Content="/" Click="Button_Click"/>
+            <Button Content="4" Click="Button_Click"/>
+            <Button Content="5" Click="Button_Click"/>
+            <Button Content="6" Click="Button_Click"/>
+            <Button Content="*" Click="Button_Click"/>
+            <Button Content="1" Click="Button_Click"/>
+            <Button Content="2" Click="Button_Click"/>
+            <Button Content="3" Click="Button_Click"/>
+            <Button Content="-" Click="Button_Click"/>
+            <Button Content="0" Click="Button_Click"/>
+            <Button Content="C" Click="Clear_Click"/>
+            <Button Content="=" Click="Button_Click"/>
+            <Button Content="+" Click="Button_Click"/>
+        </UniformGrid>
+    </Grid>
+</Window>
