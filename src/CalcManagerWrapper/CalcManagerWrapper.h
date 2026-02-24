8c310e0bf79ccfd62736a26112f377e143ba827e
Quevedo-crack <quevedotropical@gmail.com>
2026-02-24 15:07:22 -0500

    Update CalcManagerWrapper.h

--- src/CalcManagerWrapper/CalcManagerWrapper.h
+++ src/CalcManagerWrapper/CalcManagerWrapper.h
@@ -1,7 +1,7 @@
 #pragma once
 using namespace System;
 
-class StandardCalculatorManager; // forward declaration del motor nativo
+class StandardCalculatorManager; // forward declaration
 
 namespace CalcManagerWrapper
 {
