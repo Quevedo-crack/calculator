55d918f87ea37bf50d648cca093b61921bb473f6
Quevedo-crack <quevedotropical@gmail.com>
2026-02-24 14:46:37 -0500

    Update CalcManagerWrapper.cpp

--- src/CalcManagerWrapper/CalcManagerWrapper.cpp
+++ src/CalcManagerWrapper/CalcManagerWrapper.cpp
@@ -1,7 +1,13 @@
 #include "CalcManagerWrapper.h"
 
-// Evitar conflicto con IServiceProvider de .NET
+// Evitar conflicto entre COM y .NET IServiceProvider
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 #undef IServiceProvider
+
+#include <windows.h>
+#include <objbase.h>
+#include <servprov.h>
 
 #include "..\\CalcManager\\CalculatorManager.h"
 #include <msclr/marshal_cppstd.h>
